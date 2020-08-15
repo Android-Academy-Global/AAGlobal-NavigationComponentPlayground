1da0286809054fbc20d336cbb4aa79756b683586
Pavel Strelchenko <p.strelchenko@hh.ru>
2020-08-16 01:03:05 +0400

    Wrong navigation into auth flow from profile tab

--- app/src/main/java/com/aaglobal/jnc_playground/ui/tabs/profile/ProfileContainerFragment.kt
+++ app/src/main/java/com/aaglobal/jnc_playground/ui/tabs/profile/ProfileContainerFragment.kt
@@ -1,8 +1,21 @@
 package com.aaglobal.jnc_playground.ui.tabs.profile
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.aaglobal.jnc_playground.R
+import kotlinx.android.synthetic.main.fragment_profile_container.*
 
 
 class ProfileContainerFragment : Fragment(R.layout.fragment_profile_container) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        fragment_profile_container__button__open_auth_flow.setOnClickListener {
+            findNavController().navigate(R.id.action__ProfileContainerFragment__to__AuthFlow)
+        }
+    }
+
 }
