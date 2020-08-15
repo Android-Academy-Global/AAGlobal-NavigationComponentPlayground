89540063cda1d69f437e36841fa59959e0bf8841
Pavel Strelchenko <p.strelchenko@hh.ru>
2020-08-16 01:42:54 +0400

    Use "root" NavController for navigation from MainFragment to Auth flow

--- app/src/main/java/com/aaglobal/jnc_playground/ui/tabs/profile/ProfileContainerFragment.kt
+++ app/src/main/java/com/aaglobal/jnc_playground/ui/tabs/profile/ProfileContainerFragment.kt
@@ -3,7 +3,7 @@ package com.aaglobal.jnc_playground.ui.tabs.profile
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
+import androidx.navigation.Navigation
 import com.aaglobal.jnc_playground.R
 import kotlinx.android.synthetic.main.fragment_profile_container.*
 
@@ -14,7 +14,8 @@ class ProfileContainerFragment : Fragment(R.layout.fragment_profile_container) {
         super.onViewCreated(view, savedInstanceState)
 
         fragment_profile_container__button__open_auth_flow.setOnClickListener {
-            findNavController().navigate(R.id.action__MainFragment__to__AuthFlow)
+            Navigation.findNavController(requireActivity(), R.id.activity_root__fragment__nav_host)
+                .navigate(R.id.action__MainFragment__to__AuthFlow)
         }
     }
 
