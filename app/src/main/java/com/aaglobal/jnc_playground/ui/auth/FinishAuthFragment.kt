5394995e8b7c85c78c99515d2182ab6e318e5a30
Pavel Strelchenko <p.strelchenko@hh.ru>
2020-08-16 00:57:50 +0400

    Add screens for authorization flow

---
+++ app/src/main/java/com/aaglobal/jnc_playground/ui/auth/FinishAuthFragment.kt
@@ -0,0 +1,15 @@
+package com.aaglobal.jnc_playground.ui.auth
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.aaglobal.jnc_playground.R
+
+
+class FinishAuthFragment : Fragment(R.layout.fragment_finish_auth) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
---
+++ app/src/main/java/com/aaglobal/jnc_playground/ui/auth/StartAuthFragment.kt
@@ -0,0 +1,14 @@
+package com.aaglobal.jnc_playground.ui.auth
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.aaglobal.jnc_playground.R
+
+class StartAuthFragment : Fragment(R.layout.fragment_start_auth) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
