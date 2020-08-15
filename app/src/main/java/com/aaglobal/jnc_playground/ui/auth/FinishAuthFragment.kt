11c269d07bb543306315bc6be4be22b754bb8ccc
Pavel Strelchenko <p.strelchenko@hh.ru>
2020-08-16 02:04:14 +0400

    Fix back navigation from auth flow

--- app/src/main/java/com/aaglobal/jnc_playground/ui/auth/FinishAuthFragment.kt
+++ app/src/main/java/com/aaglobal/jnc_playground/ui/auth/FinishAuthFragment.kt
@@ -2,7 +2,9 @@ package com.aaglobal.jnc_playground.ui.auth
 
 import android.os.Bundle
 import android.view.View
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
 import com.aaglobal.jnc_playground.R
 import kotlinx.android.synthetic.main.fragment_finish_auth.*
@@ -14,14 +16,36 @@ class FinishAuthFragment : Fragment(R.layout.fragment_finish_auth) {
         const val AUTH_FLOW_RESULT_KEY = "auth_flow_result"
     }
 
+
+    private var callback: OnBackPressedCallback? = null
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         fragment_finish_auth__button.setOnClickListener {
-            findNavController().popBackStack(R.id.auth__nav_graph, true)
+            Navigation.findNavController(
+                requireActivity(),
+                R.id.activity_root__fragment__nav_host
+            ).popBackStack(R.id.auth__nav_graph, true)
 
             findNavController().currentBackStackEntry?.savedStateHandle?.set(AUTH_FLOW_RESULT_KEY, true)
         }
+
+        callback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                Navigation.findNavController(
+                    requireActivity(),
+                    R.id.activity_root__fragment__nav_host
+                ).popBackStack()
+            }
+        }.also {
+            requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, it)
+        }
+    }
+
+    override fun onDestroyView() {
+        callback?.remove()
+        super.onDestroyView()
     }
 
 }
--- app/src/main/java/com/aaglobal/jnc_playground/ui/auth/StartAuthFragment.kt
+++ app/src/main/java/com/aaglobal/jnc_playground/ui/auth/StartAuthFragment.kt
@@ -2,12 +2,16 @@ package com.aaglobal.jnc_playground.ui.auth
 
 import android.os.Bundle
 import android.view.View
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
 import com.aaglobal.jnc_playground.R
 import kotlinx.android.synthetic.main.fragment_start_auth.*
 
 class StartAuthFragment : Fragment(R.layout.fragment_start_auth) {
+
+    private var callback: OnBackPressedCallback? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -15,6 +19,22 @@ class StartAuthFragment : Fragment(R.layout.fragment_start_auth) {
         fragment_start_auth__button.setOnClickListener {
             findNavController().navigate(R.id.action__StartAuthFragment__to__FinishAuthFragment)
         }
+
+        callback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                Navigation.findNavController(
+                    requireActivity(),
+                    R.id.activity_root__fragment__nav_host
+                ).popBackStack()
+            }
+        }.also {
+            requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, it)
+        }
+    }
+
+    override fun onDestroyView() {
+        callback?.remove()
+        super.onDestroyView()
     }
 
 }
