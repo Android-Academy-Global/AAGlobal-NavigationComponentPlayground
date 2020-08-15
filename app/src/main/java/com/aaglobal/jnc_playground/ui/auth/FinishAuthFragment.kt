2f1ef2d1853235a335fead89fbeb3fb6dbe6f9cf
Pavel Strelchenko <p.strelchenko@hh.ru>
2020-08-16 01:01:43 +0400

    Navigation between auth flow screens

--- app/src/main/java/com/aaglobal/jnc_playground/ui/auth/FinishAuthFragment.kt
+++ app/src/main/java/com/aaglobal/jnc_playground/ui/auth/FinishAuthFragment.kt
@@ -3,13 +3,25 @@ package com.aaglobal.jnc_playground.ui.auth
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.aaglobal.jnc_playground.R
+import kotlinx.android.synthetic.main.fragment_finish_auth.*
 
 
 class FinishAuthFragment : Fragment(R.layout.fragment_finish_auth) {
 
+    companion object {
+        const val AUTH_FLOW_RESULT_KEY = "auth_flow_result"
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        fragment_finish_auth__button.setOnClickListener {
+            findNavController().popBackStack(R.id.auth__nav_graph, true)
+
+            findNavController().currentBackStackEntry?.savedStateHandle?.set(AUTH_FLOW_RESULT_KEY, true)
+        }
     }
 
 }
--- app/src/main/java/com/aaglobal/jnc_playground/ui/auth/StartAuthFragment.kt
+++ app/src/main/java/com/aaglobal/jnc_playground/ui/auth/StartAuthFragment.kt
@@ -3,12 +3,18 @@ package com.aaglobal.jnc_playground.ui.auth
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.aaglobal.jnc_playground.R
+import kotlinx.android.synthetic.main.fragment_start_auth.*
 
 class StartAuthFragment : Fragment(R.layout.fragment_start_auth) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        fragment_start_auth__button.setOnClickListener {
+            findNavController().navigate(R.id.action__StartAuthFragment__to__FinishAuthFragment)
+        }
     }
 
 }
